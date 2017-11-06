5d40ca7f2f2558b7fffd873123db294977821b50
edithau <edith.au@gmail.com>
2017-11-05 22:22:15 -0800

    include id in #index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.limit(200).order('last_login desc').select('fname,lname,last_login')
+    @users = User.limit(200).order('last_login desc').select('id,fname,lname,last_login')
     render json: @users, status: 200
   end
 
