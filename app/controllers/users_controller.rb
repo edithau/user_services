8a35f180019044207929c52b4378195e5db1dd79
edithau <edith.au@gmail.com>
2017-11-06 12:15:00 -0800

    added #show endpoint, fixed routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.limit(200).order('last_login desc').select('id,fname,lname,last_login')
-    render json: @users, status: 200
+    users = User.limit(200).order('last_login desc').select('id,fname,lname,last_login')
+    render json: users, status: 200
+  end
+
+  def show
+    user = User.where(['id = ?', params[:id]]).select('id,fname,lname,last_login').first
+    render json: user.to_json, status: 200
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/recent_logged_in_users' => 'users#index', as: :recent_logged_in_users
+  resources :users, only: [:show, :index]
 
 end
