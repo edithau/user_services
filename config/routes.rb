3f457451a7dd095f862a478a1c8838f9bb1783db
edithau <edith.au@gmail.com>
2017-11-06 22:35:57 -0800

    added endpoint to support movie queue services cache preload

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :index]
-
+  get '/last_logged_in_users' => 'users#index', as: :last_logged_in_users
 end
